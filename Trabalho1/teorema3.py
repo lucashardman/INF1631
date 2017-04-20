f48020dc8fac5dc08077863242596112af74ba38
Lucas Hardman Gomes Campos Franca <l80050202@china.huawei.com>
2017-04-20 10:05:41 -0300

    melhorias no teorema 3

--- Trabalho1/teorema3.py
+++ Trabalho1/teorema3.py
@@ -1,3 +1,9 @@
+import time
+import sys
+
+TIME_MAX = 5
+
+#Função do algoritmo que estamos estudando
 def geraTorneio(k, start = 1):
     if k == 1:
         return [[(start, start+1)]]
@@ -17,39 +23,46 @@ def geraTorneio(k, start = 1):
             round.append(game)
         t.append(round)
     return t
-    
+
+#Função para printar os resultados da divisão dos jogos
 def printJogos(t):
     for round, games in enumerate(t):
         print("		Round #%02d" % (round+1))
         for game, teams in enumerate(games):
             print("		Game #%02d:		%d vs %d" % (game+1, teams[0], teams[1]))
-
-if __name__ == '__main__':
-    from time import time
-    import sys
-
-    TIME_MAX = 5
-
 	
-	
-    print("	Digite um valor k > 1 fazer um teste: ")
-    k = int(input(" k: "))
+print("	Digite um valor k > 1 fazer um teste: ")
+k = int(input(" k: "))
 
-    start = time()
-    execs = 0
-	
-    while not (time() - start > TIME_MAX):
-        execs += 1
-        resultado = geraTorneio(k, 1)
-    end = time()
-    tempoTotal = end - start
+t = 5
+start = time.time()
+execs = 0
+inicio = time.time()
+temp0 = 1
 
-    #printJogos(resultado)
-    print (" Execucoes: %d" % execs)
-    print (" Tempo: %.3fs" % tempoTotal)
-    print (" Tempo por Execucao: %.6fms" % (1000*tempoTotal/execs))
-	#pTeste = 1
-	#print("	Digite 0 caso queira ver o resultado: ")
-    #pTeste = int(input("	Digite 0 caso queira ver o resultado: "))
-	#if pTeste == 0:
-	#	printJogos(resultado)
+#Faz o teste varias vezes até completar 5 segundos
+while(time.time() - inicio < t):
+
+	execs += 1
+	resultado = geraTorneio(k, 1) #Chama a função que estamos estudando
+	temp = time.time()
+	
+	if ((round(temp0)) != (round(temp))): #Bloco apenas para printar a contagem de 5 segundos
+		temp0 = round(temp)
+		temp2 = (round(temp)) - (round(inicio))
+		if ((6 - temp2) != 6):
+			print(" Faltam %d segundos..." % (6 - temp2))
+
+end = time.time()
+tempoTotal = end - start
+
+#Printa o resultado do teste do algoritmo
+print ("\n Execucoes: %d" % execs)
+print (" Tempo: %.3fs" % tempoTotal)
+print (" Tempo por Execucao: %.6fms\n" % (1000*tempoTotal/execs))
+
+#Printa o resultado da divisão dos jogos
+pTeste = 1
+pTeste = str(input(" Digite 0 caso queira ver o resultado: "))
+if pTeste == "0":
+	printJogos(resultado)
