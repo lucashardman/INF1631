cd1748161523d6c198093920638cfc7faca8a3b4
Stephanie Fay <stephaniemfay@gmail.com>
2017-07-03 18:18:34 -0300

    corrections in problem 3

--- Trabalho2/teorema3.py
+++ Trabalho2/teorema3.py
@@ -25,14 +25,88 @@ def read_file (file):
 			if len(line) > 0:
 				if (line[0] == "T") and (len(line) == 1):
 					flag = 1
-				if line[0] == "W":
+				if (line[0] == "W"):
 					flag = 2
 					counter = int(line[2])
 	return content
 
 
 args = read_file(sys.argv)
-print("\n\n")
+type = sys.argv[2]
+
+if type == "a":
+	print("Item a:")
+elif type == "b":
+	print("Item b:")
+elif type == "c":
+	print("Item c:")
+
+for x in range (0, len(args)):
+	print("\nBuscando na string: " + args[x][0] + "\n")
+	
+	for y in range(0, len(args[x][1])):
+		print(">>> " + args[x][1][y])
+		positions = []
+		aux = 0
+		if type == "a":
+			aux = 0
+			while (args[x][0].find(args[x][1][y], aux) > 0):
+				aux = args[x][0].find(args[x][1][y], aux)
+				positions.append(aux)
+				aux = aux + 1
+
+			print("Foram achadas " + str(len(positions)) + " correspondências.")
+		if type == "b":
+			args[x][0] = args[x][0].upper()
+			args[x][1][y] = args[x][1][y].upper()
+
+			aux = 0
+			while (args[x][0].find(args[x][1][y], aux) > 0):
+				aux = args[x][0].find(args[x][1][y], aux)
+				positions.append(aux)
+				aux = aux + 1
+
+			print("Foram achadas " + str(len(positions)) + " correspondências.")
+
+		if type == "c":
+			temp = []
+			for i in range (0, len(args[x][1][y])):
+				aux = list(args[x][1][y])
+				aux.pop(i)
+				aux = "".join(aux)
+				temp.append(aux)
+
+			for i in range (0, len(temp)):
+				aux = 0
+				positions = []
+				while (args[x][0].find(temp[i], aux) > 0):
+					aux = args[x][0].find(temp[i], aux)
+					positions.append(aux)
+					aux = aux + 1
+
+				print("Foram achadas " + str(len(positions)) + " correspondências para " + temp[i])
+
+			args[x][0] = args[x][0].upper()
+			args[x][1][y] = args[x][1][y].upper()
+			temp = []
+			for i in range (0, len(args[x][1][y])):
+				aux = list(args[x][1][y])
+				aux.pop(i)
+				aux = "".join(aux)
+				temp.append(aux)
+
+			for i in range (0, len(temp)):
+				aux = 0
+				positions = []
+				while (args[x][0].find(temp[i], aux) > 0):
+					aux = args[x][0].find(temp[i], aux)
+					positions.append(aux)
+					aux = aux + 1
+
+				print("Foram achadas " + str(len(positions)) + " correspondências para " + temp[i])
+
+
+"""print("\n\n")
 for x in range (0, len(args)):
 	print("Buscando na string: " + args[x][0] + "\n")
 	for y in range(0, len(args[x][1])):
@@ -85,4 +159,4 @@ for x in range (0, len(args)):
 			aux = aux + 1
 
 		print("Foram achadas " + str(len(positions)) + " correspondências.")
-		print("\n")
+		print("\n")"""
