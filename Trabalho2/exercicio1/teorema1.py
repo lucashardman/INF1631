0d5a36084584b59cb82c0819328fc69106e00070
Lucas <lucas.hardman92@gmail.com>
2017-07-03 20:12:45 -0300

    reorganizado

--- Trabalho2/exercicio1/teorema1.py
+++ Trabalho2/exercicio1/teorema1.py
@@ -1,9 +1,6 @@
-# -*- coding: utf-8 -*-
-
 from pygraph.classes.graph import graph
 
 def teo_1(g, k):
-  # Salvaguarda
   if k > len(g.nodes()):
     raise ValueError('FORBIDDEN: K > |V|')
   if k <= 0:
@@ -18,37 +15,26 @@ def teo_1(g, k):
   # Hipotese indutiva
   tree = teo_1(g, k-1)
 
-  # V - V_k
   all_nodes = g.nodes()
   used_nodes = tree.nodes()
   external_nodes = [node for node in all_nodes if node not in used_nodes]
 
-  # Conjunto F de arestas possiveis
   r = []
   for used_node in used_nodes:
     for external_node in external_nodes:
       if g.has_edge((used_node, external_node)):
         r.append((used_node, external_node))
 
-  # Aresta de maior peso em F
   new_edge = max(r, key=lambda e: g.edge_weight(e))
 
-  # V_{k+1} = V_k + {b}
   a, b = new_edge
   tree.add_node(b)
 
-  # E_{k+1} = E_k + {(a, b)}
   tree.add_edge(new_edge)
 
   return tree
 
 if __name__ == '__main__':
-  # Codigo usado para testar com diferentes valores de x, y, e k
-  # Uso:
-  #   python teo1.py k arq.tsp
-  #     k - o parâmetro K do teorema
-  #     arq.tsp - arquivo contendo uma tupla (id, x, y) por linha
-       
   import tsp_reader
   import sys
   from time import time
@@ -67,7 +53,6 @@ if __name__ == '__main__':
   if k > len(g.nodes()):
     raise ValueError('FORBIDDEN: K > |V|')
 
-  # Executa EXECS_PER_LOOP vezes a cada vez, ate passar de TIME_THRESHOLD segundos
   start = time()
   execs = 0
   while time() - start < TIME_THRESHOLD:
@@ -80,4 +65,3 @@ if __name__ == '__main__':
   print("k = %d" % k)
   print("file = %s" % tsp_file)
   print("time/exec = %.6f ms" % (1000*elapsed/execs))
-print
